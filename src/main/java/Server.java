d6e669a66a0e615ca6881bd1cb37f1552f042c54
Haruna Kaze <haruna.kaze@gmail.com>
2019-11-06 23:52:57 +0700

    Initial Commit

---
+++ src/main/java/Server.java
@@ -0,0 +1,74 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+
+import org.java_websocket.WebSocket;
+import org.java_websocket.handshake.ClientHandshake;
+import org.java_websocket.server.WebSocketServer;
+
+public class Server extends WebSocketServer {
+
+    public Server(InetSocketAddress address) {
+        super(address);
+    }
+
+    @Override
+    public void onOpen(WebSocket conn, ClientHandshake handshake) {
+        conn.send("Welcome to the server!"); //This method sends a message to the new client
+        broadcast( "new connection: " + handshake.getResourceDescriptor() ); //This method sends a message to all clients connected
+        System.out.println("new connection to " + conn.getRemoteSocketAddress());
+    }
+
+    @Override
+    public void onClose(WebSocket conn, int code, String reason, boolean remote) {
+        System.out.println("closed " + conn.getRemoteSocketAddress() + " with exit code " + code + " additional info: " + reason);
+    }
+
+    @Override
+    public void onMessage(WebSocket conn, String message) {
+        System.out.println("received message from "	+ conn.getRemoteSocketAddress() + ": " + message);
+    }
+
+    @Override
+    public void onMessage( WebSocket conn, ByteBuffer message ) {
+        System.out.println("received ByteBuffer from "	+ conn.getRemoteSocketAddress());
+    }
+
+    @Override
+    public void onError(WebSocket conn, Exception ex) {
+        System.err.println("an error occurred on connection " + conn.getRemoteSocketAddress()  + ":" + ex);
+    }
+
+    @Override
+    public void onStart() {
+        System.out.println("server started successfully");
+    }
+
+
+    public static void main(String[] args) {
+        String host = "localhost";
+        int port = 8887;
+
+        WebSocketServer server = new Server(new InetSocketAddress(host, port));
+        server.start();
+
+        BufferedReader sysin = new BufferedReader( new InputStreamReader( System.in ) );
+        while ( true ) {
+            try {
+                String in = sysin.readLine();
+
+                server.broadcast( in );
+                if( in.equals( "exit" ) ) {
+                    server.stop(1000);
+                    break;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
